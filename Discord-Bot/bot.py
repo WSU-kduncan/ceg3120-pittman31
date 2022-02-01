81712a53cc9d7a63dec46aa36295b944e62c4862
Zachary Pittman <pittman.31@wright.edu>
2022-02-01 19:53:14 +0000

    Added functionality to roll a d20

--- Discord-Bot/bot.py
+++ Discord-Bot/bot.py
@@ -50,4 +50,35 @@ async def on_message(message):
         response = random.choice(hitchhiker_quotes)
         await message.channel.send(response)
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    numbers = [
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        '11',
+        '12',
+        '13',
+        '14',
+        '15',
+        '16',
+        '17',
+        '18',
+        '19',
+        '20'
+    ]
+
+    if message.content == '!rd20':
+        response = random.choice(numbers)
+        await message.channel.send(response)
 client.run(TOKEN)
